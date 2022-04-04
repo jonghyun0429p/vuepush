cf29f52fb944354fc159c632f50f93722143992c
jonghyun0429p <bwhdgus0429@naver.com>
2022-04-04 23:26:13 +0900

    hmm

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container; background" style="font-family: serif">
+    <form @submit.prevent="submitForm">
+      <div>회원가입 페이지</div>
+      <div>
+        <label for="email">email</label>
+        <input type="text" id="email" v-model="email"/>
+      </div>
+      <div>
+        <label for="name">이름</label>
+        <input type="text" id="name" v-model="name"/>
+      </div>
+      <div>
+        <label for="password">비밀번호</label>
+        <input type="password" id="password" v-model="password"/>
+      </div>
+      <div>
+        <label for="passwordConfirm">비밀번호확인</label>
+        <input type="password" id="passwordConfirm" v-model="passwordConfirm"/>
+      </div>
+      <button style="padding: 2px 10px" type="submit">확인</button>
+    </form>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: 'SignupForm',
+  data() {
+    return{
+      email: '',
+      name: '',
+      password: '',
+      passwordConfirm: '',
+    };
+  },
+  methods: {
+    submitForm() {
+      console.log('dd')
+    },
+  },
+};
 </script>
 
 <style>
@@ -21,6 +53,26 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+.background {
+  height: 100vh;
+  overflow: hidden;
+  margin: 0;
+  background-color: beige;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
+.css {
+  width: 1600px;
+  height: 50px;
+  background: rgb(219, 219, 245);
+  text-align: center;
+  line-height: 30px;
+  box-sizing: border-box;
+}
+
+
 </style>
